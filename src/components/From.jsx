3e318774debe035176ed8d3fdeb4f11527c97503
Bhaktesh604 <bhakteshgadhiya604@gmail.com>
2024-08-07 16:46:53 +0530

    change all

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -227,7 +227,7 @@ const From = () => {
                                     </div>
                                     <div className="col-12 col-md-6 ps-0  ">
                                         <div className="me-0 me-xl-2 me-xxl-4">
-                                            <div className="contact_sub_boxx d-xxl-flex col-12 text-center text-xxl-start border rounded-2 mb-4">
+                                            <div className="contact_sub_boxx d-xxl-flex col-12 text-center text-xxl-start border rounded-2 mb-4 mb-md-0">
                                                 <div className="contact_icons_bg me-xxl-3 d-flex justify-content-center mb-3 mb-xxl-0">
                                                     <FaPhone className=' d-flex text-light  justify-content-center align-items-center rounded-3 p-2' />
                                                 </div>
@@ -240,7 +240,7 @@ const From = () => {
                                     </div>
                                     <div className="col-12 col-md-6 ps-0 ">
                                         <div className="me-0 me-md-3 me-xl-2 me-xxl-4">
-                                            <div className="contact_sub_boxx  d-xxl-flex col-12 text-center text-xxl-start border rounded-2 ">
+                                            <div className="contact_sub_boxx  d-xxl-flex col-12 text-center text-xxl-start border rounded-2 mb-4 mb-md-0">
                                                 <div className="contact_icons_bg me-xxl-3 d-flex justify-content-center mb-3 mb-xxl-0">
                                                     <FaClock className='d-flex text-light  justify-content-center align-items-center rounded-3 p-2' />
                                                 </div>
@@ -265,8 +265,8 @@ const From = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-12 d-flex flex-column flex-lg-row  justify-content-between py-5 ps-3 py-sm-4  ps-sm-4 pe-0 align-items-center border-top">
-                                    <h6 className="fw-bold mb-0">Social Media:</h6>
+                                <div className="col-12 d-flex flex-column flex-lg-row  mt-5 mt-xl-0 justify-content-between py-5 ps-3 py-sm-4  ps-sm-4 pe-0 align-items-center border-top">
+                                    <h6 className="fw-bold mb-4 mb-md-0">Social Media:</h6>
                                     <ul className="social_main_menu d-flex ps-0 mb-0 ">
                                         <li className="me-3">
                                             <Link to="https://www.facebook.com/profile.php?id=61559410317814" target='_blank' className="border rounded-3 d-flex align-items-center">
