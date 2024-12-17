fc939d5e156de6bb7f8954937c6e9828ffd03a9f
Bhaktesh604 <bhakteshgadhiya604@gmail.com>
2024-12-17 13:05:49 +0530

    change all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Children, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -28,7 +28,10 @@ import Career from "./pages/career";
 // import { useEffect } from 'react'
 import Thankyou from "./components/thankyou";
 import Notfound from "./pages/notfound";
+import { BeatLoader } from "react-spinners";
 const Layout = ({ children }) => {
+
+  
   return (
     <>
       <Header />
@@ -43,32 +46,33 @@ function App() {
     const wow = new WOW();
     wow.init(); // Initialize WOW.js
   }, []);
-
+ 
   return (
-    <Router>
-   
-      <Routes>
-        <Route path="/" element={ <Layout><Home /></Layout> } />
-        <Route path="/portfolio" element={ <Layout><Portfolio /></Layout> } />
-        <Route path="/portfolio/:portfolioid" element={ <Layout><Portfolioview /></Layout> } />
-        <Route path="/company" element={ <Layout><Company /></Layout> } />
-        <Route path="/contact" element={ <Layout><Contact /></Layout> } />
-        <Route path="/service" element={ <Layout><Service /></Layout> } />
-        <Route path="/service/webdevelopment" element={ <Layout><Webdevelopment /></Layout> } />
-        <Route path="/service/graphic" element={ <Layout><Graphic /></Layout> } />
-        <Route path="/service/uiux" element={ <Layout><Uiux /></Layout> } />
-        <Route path="/service/mobileapps" element={ <Layout><MobileApp /></Layout>}  />
-        <Route path="/service/digital" element={ <Layout><Digital /></Layout> } />
-        <Route path="/blog" element={ <Layout><Blog /></Layout> } />
-        <Route path="/blog/businesspage" element={ <Layout><Businesspage /></Layout> } />
-        <Route path="/Career" element={ <Layout><Career /></Layout> } />
-        <Route path="/thankyou" element={ <Layout><Thankyou /></Layout> } />
-        <Route path="*" element={<Notfound />} />
-        {/* <Route path='/blog/bussiness' element={<Bussines/>} /> */}
-      </Routes>
+      <Router>
      
-    </Router>
-  );
+        <Routes>
+          <Route path="/" element={ <Layout><Home /></Layout> } />
+          <Route path="/portfolio" element={ <Layout><Portfolio /></Layout> } />
+          <Route path="/portfolio/:portfolioid" element={ <Layout><Portfolioview /></Layout> } />
+          <Route path="/company" element={ <Layout><Company /></Layout> } />
+          <Route path="/contact" element={ <Layout><Contact /></Layout> } />
+          <Route path="/service" element={ <Layout><Service /></Layout> } />
+          <Route path="/service/webdevelopment" element={ <Layout><Webdevelopment /></Layout> } />
+          <Route path="/service/graphic" element={ <Layout><Graphic /></Layout> } />
+          <Route path="/service/uiux" element={ <Layout><Uiux /></Layout> } />
+          <Route path="/service/mobileapps" element={ <Layout><MobileApp /></Layout>}  />
+          <Route path="/service/digital" element={ <Layout><Digital /></Layout> } />
+          <Route path="/blog" element={ <Layout><Blog /></Layout> } />
+          <Route path="/blog/businesspage" element={ <Layout><Businesspage /></Layout> } />
+          <Route path="/Career" element={ <Layout><Career /></Layout> } />
+          <Route path="/thankyou" element={ <Layout><Thankyou /></Layout> } />
+          <Route path="*" element={<Notfound />} />
+          {/* <Route path='/blog/bussiness' element={<Bussines/>} /> */}
+        </Routes>
+       
+      </Router>
+    )
+  
 }
 
 export default App;
